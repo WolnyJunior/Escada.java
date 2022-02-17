7c44693e7136df7c3ae10e286581f470ad33882c
Wolny Junior <93688264+WolnyJunior@users.noreply.github.com>
2022-02-17 18:46:04 -0300

    Add files via upload

---
+++ Escada.java
@@ -0,0 +1,10 @@
+public class Escada{
+	public static void main(String[] args){
+	for ( int a = 0; a < 6;a++){
+		for ( int e = 0; e <= a; e++){
+			System.out.print("*");
+		}
+	System.out.println();
+	}
+	}
+}
